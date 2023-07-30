f17eb91ca0164059de575fef22984156aa61d81e
Kartavya Singh Rawat <kartavyasinghrawat1234@gmail.com>
2023-07-30 11:20:38 +0530

    added webpack build configuration for production

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "docs"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/views/index.eta", to: "index.eta" },
+        { from: "public/*", to: "public", force: true },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+    fallback: {
+      path: require.resolve("path-browserify"),
+      url: require.resolve('url/'),
+      stream: require.resolve("stream-browserify"),
+      fs: false,
+      "fs-extra": require.resolve("fs-extra"),
+      querystring: require.resolve("querystring-es3"),
+      http: require.resolve("stream-http"),
+      net: require.resolve("node-libs-browser/mock/net"),
+      crypto: require.resolve("crypto-browserify"),
+      zlib: require.resolve("browserify-zlib")
+    },
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
